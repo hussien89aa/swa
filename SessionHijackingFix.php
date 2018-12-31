7eba590edcdde9e98924219eb24a8334caaefe04
Hussein Alrubaye <hussien89aa@outlook.com>
2018-12-30 19:14:37 -0500

    update code to avoid Session Hijacking

--- SessionHijackingFix.php
+++ SessionHijackingFix.php
@@ -1,9 +1,17 @@
 <?php
-//This code to avoid Session Hijacking by making sure the request is comming from same browser and same IP address
+/*
+* This code to avoid Session Hijacking,
+* By making sure the requests is coming from same browser and same IP address for every session.
+* If session info requested from different browser or IP address, we will reject request.
+* NOTE: DONOT add session_start() anywhere.
+* If your file needs session, make sure to include this header -->  require_once 'SessionManagement.php';
+*/
+require_once 'php_includes/translation.inc';
+
 session_start();
 
-// generate token from IP and user agent
-function getToken(){
+// generate user token from IP and user agent
+function getUserPCInfo(){
     $user_agent = $_SERVER['HTTP_USER_AGENT'];
     $ip = null;
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
@@ -13,22 +21,14 @@ function getToken(){
     } else {
         $ip = $_SERVER['REMOTE_ADDR'];
     }
-//echo $ip . ":" .$user_agent;
-    return md5($ip . ":" .$user_agent);
+    return  $ip . ":" .$user_agent ;
 }
-
 // first time login generate token
 if(empty($_SESSION['tokenH'])){
-    $_SESSION['tokenH'] = getToken();
-
-}else{
-
-// if attemp to attack stop him
-    if($_SESSION['tokenH'] != getToken() ){
-
-        die("Token isnot valid");
-
+    $_SESSION['tokenH'] = password_hash(getUserPCInfo(), PASSWORD_DEFAULT);
+}else {
+    // if attack happen, or user IP changed(switch to other router), ask user to re-open the browser
+    if(!password_verify(  getUserPCInfo(),$_SESSION['tokenH'] ) ) {
+        die($translatePhrase("10462?You are not using a valid Token, close the browser and open it again"));
     }
 }
-
-?>
