addacc21727c9826ea43292bde2ef345d19bba13
Hussein Alrubaye <hussien89aa@outlook.com>
2019-04-18 23:47:13 -0400

    remove google ads

--- headerTab.php
+++ headerTab.php
@@ -8,15 +8,7 @@ header('X-XSS-Protection:0'); ?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111698541-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-111698541-1');
-    </script>
+ 
 
 <style>
 
